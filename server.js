263611e5b436de3d2874b4570c3639e70ad5d629
Vaibhav Mahendrasing Rajput <157829577+vaibhavrajput07@users.noreply.github.com>
2025-04-30 20:41:15 +0530

    set-password route disable

--- server.js
+++ server.js
@@ -34,27 +34,27 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   });
 
 // Password-related routes
-app.post('/password/set-password', async (req, res) => {
-  const { password } = req.body;
+// app.post('/password/set-password', async (req, res) => {
+//   const { password } = req.body;
 
-  if (!password) {
-    return res.status(400).json({ error: "Password is required!" });
-  }
+//   if (!password) {
+//     return res.status(400).json({ error: "Password is required!" });
+//   }
 
-  // Hash the password before saving
-  const hashedPassword = await bcrypt.hash(password, 10);
+  
+//   const hashedPassword = await bcrypt.hash(password, 10);
 
-  try {
-    const passwordRecord = new Password({
-      password: hashedPassword,
-    });
+//   try {
+//     const passwordRecord = new Password({
+//       password: hashedPassword,
+//     });
 
-    await passwordRecord.save();
-    res.status(201).json({ message: "Password set successfully!" });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to set the password" });
-  }
-});
+//     await passwordRecord.save();
+//     res.status(201).json({ message: "Password set successfully!" });
+//   } catch (err) {
+//     res.status(500).json({ error: "Failed to set the password" });
+//   }
+// });
 
 // Love-related routes
 app.post('/love/save-love', async (req, res) => {
